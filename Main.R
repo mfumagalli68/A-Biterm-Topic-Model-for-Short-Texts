957cd860aa5c19fb4fc14d451cfdde2dc50dfb73
mfumagalli68 <m.fumagalli68@gmail.com>
2018-05-20 23:57:35 +0200

    R

--- Main.R
+++ Main.R
@@ -3,16 +3,19 @@ library(topicmodels)
 library(stringi)
 library(readr)
 library (tm)
+library(dplyr)
 Survey <- read_csv("C:/Users/mauro/Dropbox/BitGram/Survey.csv")
 docs<-Survey$`La ringraziamo per il tempo che ci ha dedicato! Vuole aggiungere qualche altre considerazioni sul nostro sito?`
 
 stopwords_regex = paste(stopwords('it'), collapse = '\\b|\\b')
 docs<-gsub('[[:punct:] ]+',' ',docs)
-docs<-unique(stri_extract_all_words(docu))
-docs<-tolower(docs)
-docs<-na.omit(docs)
+docs<-unique(stri_extract_all_words(docs))
+docs<-lapply(docs,tolower)
+#docs<-na.omit(docs)
 #soptwords_regex = paste0('\\b', stopwords_regex, '\\b')
 docs = lapply(docs,stringr::str_replace_all,stopwords_regex, '')
+#eliminare gli spazi e poi siamo ok
+
 
 #words<-unlist(unique(stri_extract_all_words(docs)))
 source("C:\\Users\\mauro\\Dropbox\\BitGram\\commons.R")
@@ -22,6 +25,7 @@ fit<-list()
 k<-1
 biterm_model<-function()
 {
+ # browser()
 for (d in docs)
 {  
   if(length(d)>1)
@@ -53,7 +57,7 @@ data_to_plot<-data_to_plot%>%dplyr::rename(terms=V1,prob=V2)%>%mutate(prob=as.nu
 
 library(ggplot2)
 data_to_plot<-data_to_plot%>%group_by(topic)%>%arrange(topic)
-
+x11()
 data_to_plot %>%ungroup()%>%
   mutate(terms = reorder(terms, prob)) %>%
   ggplot(aes(terms, prob, fill = factor(topic))) +
